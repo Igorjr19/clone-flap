4c0a259bc1d0008b384297d3453cea32b8b6c905
Ígor José Rodrigues <igorjr2003@gmail.com>
2024-09-20 15:17:38 -0300

    Fix navigation

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,19 +1,40 @@
 import { Container, Nav, Navbar, Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
+  const style = ({ isActive }: { isActive: boolean }) => {
+    return {
+      textDecoration: 'none',
+      color: 'inherit',
+      fontWeight: isActive ? 'bold' : 'normal',
+    };
+  };
+
   return (
     <Container fluid>
       <Row>
         <Navbar>
           <Nav fill defaultActiveKey={'/'} activeKey={window.location.pathname}>
             <Nav.Item className="ms-3">
-              <Nav.Link href="/#/">Home</Nav.Link>
+              <Nav.Link>
+                <NavLink to="/" style={style}>
+                  Home
+                </NavLink>
+              </Nav.Link>
             </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="/#/regex">Regex</Nav.Link>
+            <Nav.Item className="ms-3">
+              <Nav.Link>
+                <NavLink to="/regex" style={style}>
+                  Regex
+                </NavLink>
+              </Nav.Link>
             </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="/#/regular-grammar">Gramática Regular</Nav.Link>
+            <Nav.Item className="ms-3">
+              <Nav.Link>
+                <NavLink to="/regular-grammar" style={style}>
+                  Gramática Regular
+                </NavLink>
+              </Nav.Link>
             </Nav.Item>
           </Nav>
         </Navbar>
